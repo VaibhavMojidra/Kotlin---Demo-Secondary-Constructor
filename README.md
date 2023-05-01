a8a1538e1320ff1a6fc521397841d53f8ed0134e
VaibhavMojidra <vmojidra25@gmail.com>
2023-05-02 03:15:08 +0530

    Secondary Constructor

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kotlin Demo Secondary Constructor
+
+In Kotlin, a secondary constructor is a way to define additional initialization logic for a class. Unlike the primary constructor, which is defined in the class header, a secondary constructor is defined inside the body of the class.
+___
+
+[![Vaibhav Mojidra - 1.jpeg](https://raw.githubusercontent.com/VaibhavMojidra/Kotlin---Demo-Secondary-Constructor/master/output/1.jpeg "Vaibhav Mojidra")](https://vaibhavmojidra.github.io/site/)
---
+++ src/Demo.kt
@@ -0,0 +1,30 @@
+class Person{
+   // Member Variables
+   var name: String
+   var age: Int
+   var salary: Double
+
+   // First Secondary Constructor
+   constructor ( _name: String, _age: Int) {
+      this.name = _name
+      this.age = _age
+      this.salary = 0.00
+      println("Name = $name")
+      println("Age = $age")
+   }
+
+   // Second Secondary Constructor
+   constructor ( _name: String, _age: Int, _salary: Double) {
+      this.name = _name
+      this.age = _age
+      this.salary = _salary
+      println("Name = $name")
+      println("Age = $age")
+      println("Salary = $salary")
+   }
+}
+
+fun main(args: Array<String>) {
+   val nuha = Person("Diya", 12)
+   val zara = Person("Vaibhav", 25, 2000000.00)
+}
